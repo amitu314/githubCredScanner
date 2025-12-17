c4bdcc1d352dbadb34c2b5560d9365bddb8b8a19
amitU <imamit314@gmail.com>
2025-12-17 16:50:06 -0500

    Update and rename checkCredonBranches.py to checkCredBranchesMultipleRepo.py

--- checkCredBranchesMultipleRepo.py
+++ checkCredBranchesMultipleRepo.py
@@ -11,30 +11,42 @@ def remoteBranchCheckNoCheckout(repoPath, regPattern):
         #branchCount = len(list(repo.branches))
         #print(f" Branch count for {repoPath}: {branchCount}")
 
-        origin.fetch()
+        origin.fetch(prune=True)
         branchCount = len(list(origin.refs))
         print(f" Branch count for {repoPath}: {branchCount}")
     except Exception as e:
-        print(f"Error fetching {e}")
+        #print(f"Error fetching {e}")
+        print(type(e), e)
+        return {}
         
     pattern = re.compile(regPattern, re.IGNORECASE | re.MULTILINE)
 
     results = {}
-    if branchCount < 25:
+    if branchCount < 500:
         for ref in origin.refs:
             
             try:
                 branch = ref.remote_head
-                print("fetching files for branch:", branch)
+                print(f"fetching files for branch {branch} | Repo: {repo}")
                 files = repo.git.ls_tree('-r', '--name-only', ref.name).splitlines()
             except Exception as e:
                 print(f"Error {e} listing files | branch {branch} | repo {repoPath}")
                 continue
+
+
+            fileCount = len(files)
+            if fileCount > 10000:
+                with open('branchTooManySkip-agent-framework.txt', 'a') as skipFile:
+                    skipFile.write(f"Skipping scan for repo {repoPath} due to large file count ({fileCount}) in branch {branch}\n")
+                print(f"Skipping repo {repoPath} — branch {branch} has {fileCount} files .")
+                continue
+                #return {}
+
             matchOnBranch = []
             for path in files:
                 
                 try:
-                    print(f"getting content: {path} ")
+                    print(f"In {repoPath}: getting content: {path} ")
                     content = repo.git.show(f"{ref.name}:{path}")
                 except Exception as e:
                     print(f"Error {e} reading file {path} | branch {branch} | repo {repoPath}")
@@ -47,26 +59,29 @@ def remoteBranchCheckNoCheckout(repoPath, regPattern):
             if matchOnBranch:
                 #print(matchOnBranch)
                 results[branch] = matchOnBranch
-                #return matchOnBranch
+                #return results
     else:
-        with open ('branchTooManySkip.txt', 'a') as skipFile:
+        with open ('branchTooManySkip-agent-framework.txt', 'a') as skipFile:
             skipFile.write(f"Skipping branch scan for {repoPath} due to high branch count: {branchCount}\n")
         #print(f"Skipping branch scan for {repoPath} due to high branch count: {branchCount}")
 
     return results
 
 
-
 if __name__ == "__main__":
-    repoPath = "./<your path to the repo/"
+    repoPath = "<path to repo>"
+    #repoPath = Path('<path to repo for Windows>')
     
     #regPattern = r"(Bearer\s+[A-Za-z0-9\.\-_]+|SECRET_KEY = \S{10,}|AccountKey=\S{10,}|AKIA[0-9A-Z]{16})"
-    regPattern = r'(Azure_Storage_(?:AccountName|AccountKey|key|Key|KEY|AccessKey|ACCESSKEY|SasToken)[^\n]+|SECRET_KEY = .{10,}|ClientSecret"\svalue=.+|(?:AccessKey|ACCESSKEY|ACCESS_KEY|Access_key)=\S{10,}|AccountKey=\S{10,}|secret_key_base:\s.[A-Za-z0-9_.-]{12,}|secret(?:\s|:|=).+[A-Za-z0-9_.-]{12,}|Bearer\s.\S{11,}| (Refresh_Token|REFRESH_TOKEN|refresh_token)\s.\S{11,}|api[_-](?:key|token)(?:\:|=).[A-Za-z0-9_.-]{10,}|-----BEGIN\s(?:RSA|DSA|EC|PGP|OPENSSH)\sPRIVATE\sKEY-----|(?:password|Password|passwd|pwd)\s*[:=]\s*[\"\'][^\s\"\']{8,}|eyJ[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,})'
-    with open ('MSGraphCreds.txt', 'w',encoding='utf-8', errors='ignore') as credFile:
+    #regPattern = r'(Azure_Storage_(?:AccountName|AccountKey|key|Key|KEY|AccessKey|ACCESSKEY|SasToken)[^\n]+|SECRET_KEY = .{10,}|AKIA[0-9A-Z]{16}|ClientSecret"\svalue=.+|(?:AccessKey|ACCESSKEY|ACCESS_KEY|Access_key)=\S{10,}|AccountKey=\S{10,}|secret_key_base:\s.[A-Za-z0-9_.-]{12,}|secret(?:\s|:|=).+[A-Za-z0-9_.-]{12,}|Bearer\s.\S{11,}| (Refresh_Token|REFRESH_TOKEN|refresh_token)\s.\S{11,}|api[_-](?:key|token)(?:\:|=).[A-Za-z0-9_.-]{10,}|-----BEGIN\s(?:RSA|DSA|EC|PGP|OPENSSH)\sPRIVATE\sKEY-----|(?:password|Password|passwd|pwd)\s*[:=]\s*[\"\'][^\s\"\']{8,}|eyJ[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,})'
+    regPattern = r'(Azure_Storage_(?:AccountName|AccountKey|key|Key|KEY|AccessKey|ACCESSKEY|SasToken)[^\n]+|SECRET_KEY(\s|:|=){0,8}(["\'`])(.{10,})(["\'`])|AKIA[0-9A-Z]{16}|ClientSecret"\svalue=(?!("ENTER_YOUR_SECRET")).+|(?:AccessKey|ACCESSKEY|ACCESS_KEY|Access_key)=\S{10,}|AccountKey=\S{10,}|secret_key_base:\s.[A-Za-z0-9_.-]{12,}|secret(\s|:|=){0,8}(["\'`])(?!(YOUR_CLIENT_SECRET|ENTER_YOUR_SECRET|value="ENTER_YOUR_SECRET"|\svalue\=\"ENTER_YOUR_SECRET\")) (.{8,})(["\'`])|Bearer\s.\S{11,}| (Refresh_Token|REFRESH_TOKEN|refresh_token)\s.\S{11,}|api[_-](?:key|token)(?:\:|=).[A-Za-z0-9_.-]{10,}|Key" value=(["\'])(?:[A-Za-z0-9=\/_\-\+]{10,})|-----BEGIN\s(?:RSA|DSA|EC|PGP|OPENSSH)\sPRIVATE\sKEY-----|(?:password|Password|passwd|pwd)\s*[:=]\s*[\"\'][^\s\"\']{8,}|eyJ[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,})'
+    
+    with open ('agentFrameworkCred.txt', 'w',encoding='utf-8', errors='ignore') as credFile:
         for root, dirs, files in os.walk(repoPath):
             for dir in dirs:
                 
                 repoPath1 = os.path.join(root, dir)
+
                 hits = remoteBranchCheckNoCheckout(repoPath1,regPattern)
                 #print(hits)
 
@@ -74,14 +89,10 @@ if __name__ == "__main__":
                     #print(f"Branch: {dir}/{branch}")
                     for item in matches:
                         for val in item['matches']:
+                            print(f"""Writing to file...""")
                             credFile.write(f"""===========================================================================================================================================================================================
 In Repo {dir} under Branch {branch}
         Found in File: {item['path']}
             Match: {val}
 """)
 
-                            '''print(f"""In Repo {dir} 
-    under Branch {branch} 
-        Found in File: {item['path']}
-            Match: {val}
-""")'''
